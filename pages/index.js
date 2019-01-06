71e8f720b189080e0db116b18605e25876d7c6f7
Eugene Zhlobo <ezhlobo@gmail.com>
2019-01-06 18:36:19 +0300

    Set title for main page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Head from 'next/head'
+
 import App from '../components/App'
 
 import '../assets/styles.css'
 
-export default () => <App />
+export default () => (
+  <React.Fragment>
+    <Head>
+      <title>Pug-in-js REPL</title>
+    </Head>
+
+    <App />
+  </React.Fragment>
+)
